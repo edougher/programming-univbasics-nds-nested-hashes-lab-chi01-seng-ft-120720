c6d1663834418e2b4c7d8aa3bffb989c071578aa
erin dougher <ejdougher@gmail.com>
2020-11-28 07:57:53 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,7 +19,7 @@ def literal_aoh
   array_2 = [
     { :name => "Don Gately", :occupation => "Live-in Staff" },
     { :name => "Joelle van Dyne", :occupation => "Radio Personality" },
-    { :name => "Pat Monteseian", :occupation => "Staff" }
+    { :name => "Pat Monteseian", :occupation => "Staff" },
     { :name => "Kate Gompert", :occupation => "None" },
     { :name => "Bruce Green", :occupation => "Fan of Mildred" }
   ]
